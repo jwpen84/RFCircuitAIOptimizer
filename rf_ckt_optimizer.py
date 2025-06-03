9273b76e073a56ed25b6136f50d9d8b87621ed76
JungwooSung <jwpen@hitel.net>
2025-06-03 19:50:20 +0900

    inductor와 resistor도 완료료

--- rf_ckt_optimizer.py
+++ rf_ckt_optimizer.py
@@ -19,6 +19,28 @@ class RFCircuitOptimizer():
         s_mat[:, 1, 0] = (2j * self.w * cap * (z0_0.real * z0_1.real)**0.5) / denom
         return s_mat
     
+    def inductor(self, ind: torch.Tensor):
+        s_mat = torch.zeros((self.frequency.shape[0], 2, 2), dtype=torch.complex64)
+        z0_0 = 50.0+0.0j
+        z0_1 = 50.0+0.0j
+        denom = (1j * self.w * ind) + (z0_0 + z0_1)
+        s_mat[:, 0, 0] = (1j * self.w * ind - z0_0.conjugate() + z0_1) / denom
+        s_mat[:, 1, 1] = (1j * self.w * ind + z0_0 - z0_1.conjugate()) / denom
+        s_mat[:, 0, 1] = 2 * (z0_0.real * z0_1.real)**0.5 / denom
+        s_mat[:, 1, 0] = 2 * (z0_0.real * z0_1.real)**0.5 / denom
+        return s_mat
+    
+    def resistor(self, res: torch.Tensor):
+        s_mat = torch.zeros((self.frequency.shape[0], 2, 2), dtype=torch.complex64)
+        z0_0 = 50.0+0.0j
+        z0_1 = 50.0+0.0j
+        denom = (z0_0 + z0_1) + res
+        s_mat[:, 0, 0] = (res - z0_0.conjugate() + z0_1) / denom
+        s_mat[:, 1, 1] = (res + z0_0 - z0_1.conjugate()) / denom
+        s_mat[:, 0, 1] = 2 * (z0_0.real * z0_1.real)**0.5 / denom
+        s_mat[:, 1, 0] = 2 * (z0_0.real * z0_1.real)**0.5 / denom
+        return s_mat
+    
 
 class MSELoss(torch.nn.Module):
     def __init__(self):
